f89846667d4902af7dbd131bff825e0d6017ce27
Wouter van Atteveldt <wouter@vanatteveldt.com>
2016-02-14 21:35:51 +0100

    Send text as utf-8

--- nlpipe/tasks.py
+++ nlpipe/tasks.py
@@ -7,7 +7,7 @@ from .celery import app
 def morphosyntactic(text):
     cmd = "$NEWSREADER_HOME/run_parser.sh"
     p2 = Popen(cmd, stdin=PIPE, stdout=PIPE, shell=True)
-    (out, err) = p2.communicate(text)
+    (out, err) = p2.communicate(text.encode("utf-8"))
     if p2.returncode != 0:
         raise Exception("Error on calling shell command: {cmd} (see logs)".format(**locals()))
     return out
